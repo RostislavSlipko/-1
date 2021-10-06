fd525fcc8241f345712934164a42a1650935c736
RostislavSlipko <91022169+RostislavSlipko@users.noreply.github.com>
2021-10-06 19:18:44 +0300

    Add files via upload

---
+++ 2/Code 2/Code 2.cpp
@@ -0,0 +1,10 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "\t ----\n";
+	cout << "\t|    |\n";
+	cout << "\t|    |\n";
+	cout << "\t ----\n";
+	return 0;
+}
---
+++ 2/Code 3/Code 3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "        Фамилия : Слипко\n";
+	cout << "            Имя : Ростислав\n";
+	cout << "       Отчество : Юрьевич\n";
+	cout << "            Пол : Мужской\n";
+	cout << "  Дата рождения : 08.02.2003\n";
+	cout << "          Адрес : One, Microsoft Way, 1, NY, USA\n";
+	cout << " Номер телефона : 2613453\n";
+	cout << "    Место учёбы : DNU\n";
+	cout << "          Класс : -\n";
+	cout << "          Хобби : Програмирование\n";
+	return 0;
+}
